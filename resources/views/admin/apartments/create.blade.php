cc16e46129fb8cdc6c3b24a88865e0e552af30e4
MatteoGiuntella <matteo.giuntella@virgilio.it>
2024-04-05 16:21:17 +0200

    fix style create

--- resources/views/admin/apartments/create.blade.php
+++ resources/views/admin/apartments/create.blade.php
@@ -69,19 +69,19 @@
         </div>
     @endif
     <div class=" container ">
-        <div class="mb-4">
+        <div class="mb-4 d-flex justify-content-end ">
             <a href="{{ route('admin.apartments.index') }}" class="btn btn-primary">
                 Torna alla Home
             </a>
         </div>
 
-        <h1>Create Apartment</h1>
+        <h1 class=" text-danger ">Crea il tuo appartamento</h1>
         <form id="myForm" action="{{ route('admin.apartments.store') }}" method="POST" enctype="multipart/form-data">
             @csrf
             <div class="row my-3">
                 <div class="col-6">
                     <div class="mb-3">
-                        <label for="name" class="form-label ">Add name</label>
+                        <label for="name" class="form-label ">Aggiungi il nome</label>
                         <input value="{{ old('name') }}" class="form-control @error('name') is-invalid @enderror"
                             type="text" id="name" name="name" maxlength="64" required>
                         @error('name')
@@ -93,10 +93,10 @@
                 </div>
                 <div class="col-6">
                     <div class="mb-3">
-                        <label for="type_of_accomodation" class="form-label">Type of Accomodation</label>
+                        <label for="type_of_accomodation" class="form-label">Scegli il tipo</label>
                         <select name="type_of_accomodation" id="type_of_accomodation" class="form-select" required>
                             <option value="" {{ old('type_of_accomodation') == null ? 'selected' : '' }}>
-                                Add Type of Accomodation
+                                Scegli il tipo
                             </option>
                             @for ($i = 0; $i < count($accomodation); $i++)
                                 <option value="{{ $accomodation[$i] }}"
@@ -118,7 +118,7 @@
             <div class="row my-3">
                 <div class="col-6">
                     <div class="mb-3">
-                        <label for="n_guests">Number Guests(between 1 and 10):</label>
+                        <label for="n_guests">Numero persone ammesse(tra 1 e 10):</label>
                         <input value="{{ old('n_guests') }}" class="form-control @error('n_guests') is-invalid @enderror"
                             type="number" id="n_guests" name="n_guests" min="1" max="10" required>
                         @error('n_guests')
@@ -130,7 +130,7 @@
                 </div>
                 <div class="col-6">
                     <div class="mb-3">
-                        <label for="n_baths">Number Baths (between 1 and 3):</label>
+                        <label for="n_baths">Numero bagni (tra 1 e 3):</label>
                         <input value="{{ old('n_baths') }}" class="form-control @error('n_baths') is-invalid @enderror"
                             type="number" id="n_baths" name="n_baths" min="1" max="3" required>
                         @error('n_baths')
@@ -145,7 +145,7 @@
             <div class="row my-3">
                 <div class="col-6">
                     <div class="mb-3">
-                        <label for="n_rooms">Number Rooms(between 1 and 6):</label>
+                        <label for="n_rooms">Numero camere(tra 1 e 6):</label>
                         <input value="{{ old('n_rooms') }}" class="form-control @error('n_rooms') is-invalid @enderror"
                             type="number" id="n_rooms" name="n_rooms" min="1" max="6" required>
                         @error('n_rooms')
@@ -157,7 +157,7 @@
                 </div>
                 <div class="col-6">
                     <div class="mb-3">
-                        <label for="n_beds">Number Beds (between 1 and 9):</label>
+                        <label for="n_beds">Posti letto (tra 1 e 9):</label>
                         <input value="{{ old('n_beds') }}" class="form-control @error('n_beds') is-invalid @enderror"
                             type="number" id="n_beds" name="n_beds" min="1" max="9" required>
                         @error('n_beds')
@@ -171,7 +171,7 @@
             <div class="row my-3">
                 <div class="col-6">
                     <div class="mb-3">
-                        <label for="mq">Mq(between 20mq and 150mq):</label>
+                        <label for="mq">Metri quadri(tra 20mq e 150mq):</label>
                         <input value="{{ old('mq') }}" class="form-control @error('mq') is-invalid @enderror"
                             type="number" id="mq" name="mq" min="20" max="150" required>
                         @error('mq')
@@ -184,7 +184,7 @@
                 </div>
                 <div class="col-6">
                     <div class="mb-3">
-                        <label for="price">Price(between 1.00$ and 1000.00$):</label>
+                        <label for="price">Prezzo per notte(tra 1.00$ e 1000.00$):</label>
                         <input value="{{ old('price') }}" class="form-control @error('price') is-invalid @enderror"
                             type="number" id="price" name="price" min="1.00" max="1000.00" required>
                         @error('price')
@@ -197,7 +197,7 @@
             </div>
             <div class="row my-3">
                 <div class="col-12">
-                    <span >Choose Services:</span>
+                    <span >Scegli i tuoi servizi:</span>
                     <div class=" py-3 ">    
                         @foreach ($services as $service)
                             <div class="form-check form-check-inline">
@@ -216,7 +216,7 @@
             <div class="row my-3">
                 <div class="col-6">
                     <div class="mb-3 position-relative">
-                        <label for="address" class="form-label ">Add address</label>
+                        <label for="address" class="form-label ">Aggiungi indirizzo</label>
                         <input value="{{ old('address') }}" class="form-control @error('address') is-invalid @enderror"
                             type="text" id="address-input" name="address" maxlength="64" autocomplete="off">
                         <ul id="suggestions">
@@ -231,7 +231,7 @@
                 </div>
                 <div class="col-6">
                     <div class="mb-3">
-                        <label for="img_cover_path" class="form-label">Apartment image</label>
+                        <label for="img_cover_path" class="form-label">Carica immagine di copertina</label>
                         <input class="form-control" type="file" id="img_cover_path" name="img_cover_path">
                     </div>
                     <div class=" my-4 ">
@@ -243,11 +243,11 @@
                         <div class="form-check form-check-inline p-0 ">
                             <input type="radio" class="btn-check" name="availability" id="success-outlined"
                                 autocomplete="off" value="1">
-                            <label class="btn btn-outline-success" for="success-outlined">Available</label>
+                            <label class="btn btn-outline-success" for="success-outlined">Disponibile</label>
 
                             <input type="radio" class="btn-check" name="availability" id="danger-outlined"
                                 autocomplete="off" value="0">
-                            <label class="btn btn-outline-danger" for="danger-outlined">Not Available</label>
+                            <label class="btn btn-outline-danger" for="danger-outlined">Non disponibile</label>
                           
                         </div>
 
@@ -257,7 +257,7 @@
             <div class="row">
                 <div class="col-12 d-flex  justify-content-center ">
                     <button type="submit" class="btn btn-success w-25">
-                        Create
+                        Crea
                     </button>
                 </div>
             </div>
